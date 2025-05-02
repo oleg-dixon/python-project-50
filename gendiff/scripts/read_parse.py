5622caf04f5d184ca60ce84ad48b18705c10e5ff
oleg_dixon <olegdixon5973@gmail.com>
2025-05-02 15:28:04 +0500

    add read_parse.py

---
+++ gendiff/scripts/read_parse.py
@@ -0,0 +1,22 @@
+import json
+
+def read_file(file_path):
+    """Читает и возвращает содержимое файла как строку."""
+    with open(file_path, 'r') as file:
+        return file.read()
+    
+
+def parse_data(data, file_path):
+    """Парсит строку данных в зависимости от указанного формата."""
+    if file_path == 'json':
+        return json.loads(data)
+
+
+def parse_file(file_path, file_format):
+    """Комбинированная функция чтения и парсинга файла."""
+    data = read_file(file_path)
+    return parse_data(data, file_format)
+
+
+
+print(parse_file('/home/dixon/python-project-50/file2.json', 'json'))
