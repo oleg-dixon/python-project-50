ec6f86ed258214803e093ff6c643bbe590312ef3
oleg_dixon <olegdixon5973@gmail.com>
2025-05-08 20:51:14 +0500

    fix main.py

--- gendiff/main.py
+++ gendiff/main.py
@@ -5,26 +5,11 @@ from gendiff.generate_diff import generate_diff
 
 
 def main():
-    # Создаем кастомный форматтер для help
-    class CustomHelpFormatter(argparse.HelpFormatter):
-        def _format_action(self, action):
-            # Для позиционных аргументов убираем текст help
-            if not action.option_strings:
-                return super()._format_action(argparse.Action(
-                    action.option_strings,
-                    action.dest,
-                    help=argparse.SUPPRESS,
-                    metavar=action.metavar
-                ))
-            return super()._format_action(action)
-
     parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.',
-        formatter_class=CustomHelpFormatter,
-        add_help=False  # Отключаем стандартный help
+        description='Compares two configuration files and shows a difference.'
     )
 
-    # Позиционные аргументы (без текста help)
+    # Позиционные аргументы
     parser.add_argument(
         'first_file',
         metavar='first_file'
@@ -35,11 +20,6 @@ def main():
     )
 
     # Опциональные аргументы
-    parser.add_argument(
-        '-h', '--help',
-        action='help',
-        help='show this help message and exit'
-    )
     parser.add_argument(
         '-f', '--format',
         metavar='FORMAT',
